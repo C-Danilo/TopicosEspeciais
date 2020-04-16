3a34fd434d73f119da8cfb63fef6ae8e7ba04208
dlinux <dlinux@dlinux-IPMH61R1>
2020-04-16 18:45:41 -0300

    Lab02

---
+++ Lab02/src/main/java/br/fepi/si/app/TesteConexaoApp.java
@@ -0,0 +1,23 @@
+package br.fepi.si.app;
+
+import javax.persistence.Persistence;
+
+public class TesteConexaoApp {
+
+	public static void main(String[] args) {
+		
+			try {
+				Persistence.createEntityManagerFactory("exemploPU");
+				System.out.println("Conectado com sucesso");
+			}catch(Exception e){
+				System.out.println("Falha na conexão"+ e.getMessage());
+			}
+					
+
+		
+		
+		
+		
+	}
+
+}
